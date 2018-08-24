31e34075ad3f1476738c1ba79e980d76a0a98884
fwthanh <fwthanh@gmail.com>
2018-08-24 23:11:46 +0700

    add Guide view

--- Flange/Flange/ViewController/ViewController.swift
+++ Flange/Flange/ViewController/ViewController.swift
@@ -13,8 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { // in half a second...
+            self.performSegue(withIdentifier: "showGuide", sender: nil)
+        }
     }
-
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showGuide" {
+//            let otpView: OTPViewController = segue.destination as! OTPViewController
+//            otpView.userId = userDetail.id
+//            otpView.otpBlock =  { (phone) -> Void in
+//                SVProgressHUD.show()
+//                self.userDetail.mobile = phone == "" ? "0" : phone
+//                self.tableView.reloadData()
+//            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
